90236209447bcf5afc5bfdfca3d5a8da10a9766c
Felipe Sampaio <felipe.sampaio@inf.ufrgs.br>
2011-09-08 14:01:30 -0300

    TEncFastPUDecision class should be implemented to handle with the fast decision issue

---
+++ source/Lib/TLibEncoder/TEncFastPUDecision.cpp
@@ -0,0 +1,12 @@
+/* 
+ * File:   TEncFastPUDecision.cpp
+ * Author: felsamps
+ * 
+ * Created on September 6, 2011, 3:24 PM
+ */
+
+#include "TEncFastPUDecision.h"
+
+TEncFastPUDecision::TEncFastPUDecision() {
+}
+
---
+++ source/Lib/TLibEncoder/TEncFastPUDecision.h
@@ -0,0 +1,58 @@
+#ifndef _TENCFASTPUDECISION_H
+#define	_TENCFASTPUDECISION_H
+
+#include <vector>
+#include "../TLibCommon/TComMv.h"
+#include "../TLibCommon/TComDataCU.h"
+
+class TEncFastPUDecision {
+    std::vector<TComMv*> bestMv, prefMv;
+    std::vector<UInt> bestDist, prefDist;
+    Bool borderA, borderB, borderC, borderD;
+    PartSize partSize;
+    static const PredMode predMode = MODE_INTER;
+    TComDataCU *cu;
+    std::vector<UInt**> distMap; 
+
+public:
+    TEncFastPUDecision();
+    
+    /* Fast PU Decision Methods */
+    void decide();
+    void xSetCUParameters();
+        
+
+    void        setCU               (TComDataCU *pCu)           { this->cu = pCu; }
+    TComDataCU* getCU               ()                          { return this->cu; }
+
+    /* Best Matches' Information Setters and Getters */
+    void        setBestMv           (UInt block, TComMv* mv)    { this->bestMv[block] = mv; }
+    TComMv*     getBestMv           (UInt block)                { return this->bestMv[block]; }
+
+    void        setBestDist         (UInt block, UInt dist)     { this->bestDist[block] = dist; }
+    UInt        getBestDist         (UInt block)                { return this->bestDist[block]; }
+
+    /* Prefferred Matches' Information Setters and Getters */
+    void        setPrefMv           (UInt block, TComMv *mv)    { this->prefMv[block] = mv; }
+    TComMv*     getPrefMv           (UInt block)                { return this->prefMv[block]; }
+
+    void        setPrefDist         (UInt block, UInt dist)     { this->prefDist[block] = dist; }
+    UInt        getPrefDist         (UInt block)                { return this->prefDist[block]; }
+
+    /* Border flags setters and getters */
+    void        setBorderA          (Bool b)                    { this->borderA = b; }
+    void        setBorderB          (Bool b)                    { this->borderB = b; }
+    void        setBorderC          (Bool b)                    { this->borderC = b; }
+    void        setBorderD          (Bool b)                    { this->borderD = b; }
+
+    Bool        isBorderA           ()                          { return borderA; }
+    Bool        isBorderB           ()                          { return borderB; }
+    Bool        isBorderC           ()                          { return borderC; }
+    Bool        isBorderD           ()                          { return borderD; }
+
+private:
+
+};
+
+#endif	/* _TENCFASTPUDECISION_H */
+
